0ca98d940d122215feaeb00e267ed1448855874d
Vinod Shetty <mechvnod@gmail.com>
2024-07-10 23:10:01 +0530

    xdisplayname done

---
+++ src/FullNameDisplay.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+function FullNameDisplay() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [fullName, setFullName] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setFullName(`${firstName} ${lastName}`);
+  };
+
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>First Name:</label>
+          <input
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label>Last Name:</label>
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <p>Full Name: {fullName}</p>
+    </div>
+  );
+}
+
+export default FullNameDisplay;
